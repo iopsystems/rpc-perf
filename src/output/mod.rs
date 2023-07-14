dfd25862d4eede868640cab193727ca22810cc13
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-07-14 10:16:31 -0700

    Add a target_qps field to the emitted rpc-perf json (#30)

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use ahash::{HashMap, HashMapExt};
+use ratelimit::Ratelimiter;
 use serde::Serialize;
 use std::io::{BufWriter, Write};
 
@@ -300,6 +301,8 @@ struct Subscribers {
 struct JsonSnapshot {
     window: u64,
     elapsed: f64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    target_qps: Option<f64>,
     client: Client,
     pubsub: Pubsub,
 }
@@ -345,7 +348,7 @@ fn heatmap_to_buckets(heatmap: &Heatmap) -> RequestLatencies {
     }
 }
 
-pub fn json(config: Config) {
+pub fn json(config: Config, ratelimit: Option<&Ratelimiter>) {
     if config.general().json_output().is_none() {
         return;
     }
@@ -413,6 +416,7 @@ pub fn json(config: Config) {
             let json = JsonSnapshot {
                 window: window_id,
                 elapsed,
+                target_qps: ratelimit.as_ref().map(|ratelimit| ratelimit.rate()),
                 client: Client {
                     connections,
                     requests,
