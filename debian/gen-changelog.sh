cbc854096d326a3be723ae6d0ea4b5dbb69a34a8
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-07-24 12:45:14 -0700

    Add workflow to create and upload apt packages (#31)

---
+++ debian/gen-changelog.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VERSION=$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "rpc-perf") | .version')
+RELEASE=${RELEASE:-1}
+
+cat <<EOM
+rpc-perf ($VERSION-$RELEASE) $(lsb_release -sc); urgency=medium
+
+  * Automated update package for rpc-perf $VERSION
+
+ -- IOP Systems <sean@iop.systems>  $(date -R)
+EOM
