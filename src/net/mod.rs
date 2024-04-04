83861c2f98770c8a8972ed74a7be585894b01344
Brian Martin <brayniac@gmail.com>
2024-04-04 16:27:31 -0700

    feat: openssl
    Adds support for openssl as a TLS/SSL provider. Makes it the new
    default.
    
    To switch back to boringssl, the `boringssl` feature must be
    enabled at build time.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,9 +1,37 @@
+use crate::error;
 use crate::Config;
-use boring::ssl::{SslFiletype, SslMethod};
-use boring::x509::X509;
-use std::io::{Error, ErrorKind, Result};
-use tokio::io::ReadBuf;
-use tokio::io::{AsyncRead, AsyncWrite};
+
+use std::io::Result;
+
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+
+pub enum SslProvider {
+    #[cfg(feature = "boringssl")]
+    Boringssl,
+    #[cfg(feature = "openssl")]
+    Openssl,
+    Unknown,
+}
+
+// clippy doesn't realize that the default changes depending on enabled features
+// so we suppress this warning
+#[allow(clippy::derivable_impls)]
+#[allow(unreachable_code)]
+impl Default for SslProvider {
+    fn default() -> Self {
+        #[cfg(feature = "boringssl")]
+        {
+            return SslProvider::Boringssl;
+        }
+        
+        #[cfg(feature = "openssl")]
+        {
+            return SslProvider::Openssl;
+        }
+
+        SslProvider::Unknown
+    }
+}
 
 pub struct Connector {
     inner: ConnectorImpl,
@@ -11,6 +39,34 @@ pub struct Connector {
 
 impl Connector {
     pub fn new(config: &Config) -> Result<Self> {
+        if config.tls().is_none() {
+            Self::plaintext()
+        } else {
+            match SslProvider::default() {
+                #[cfg(feature = "boringssl")]
+                SslProvider::Boringssl => {
+                    Self::boringssl(config)
+                }
+                #[cfg(feature = "openssl")]
+                SslProvider::Openssl => {
+                    Self::openssl(config)
+                }
+                SslProvider::Unknown => {
+                    error!("no TLS/SSL provider could be found. Check that rpc-perf was built with either boringssl or openssl support");
+                    std::process::exit(1);
+                }
+            }
+        }
+    }
+
+    fn plaintext() -> Result<Self> {
+        Ok(Connector {
+            inner: ConnectorImpl::Tcp,
+        })
+    }
+
+    #[cfg(feature = "boringssl")]
+    fn boringssl(config: &Config) -> Result<Self> {
         if config.tls().is_none() {
             return Ok(Connector {
                 inner: ConnectorImpl::Tcp,
@@ -23,7 +79,7 @@ impl Connector {
         let certificate = tls_config.certificate();
         let certificate_chain = tls_config.certificate_chain();
 
-        let mut ssl_connector = boring::ssl::SslConnector::builder(SslMethod::tls_client())?;
+        let mut ssl_connector = boring::ssl::SslConnector::builder(boring::ssl::SslMethod::tls_client())?;
 
         if let Some(ca_file) = tls_config.ca_file() {
             ssl_connector.set_ca_file(ca_file)?;
@@ -31,16 +87,16 @@ impl Connector {
 
         // mTLS configuration
         if private_key.is_some() && (certificate.is_some() || certificate_chain.is_some()) {
-            ssl_connector.set_private_key_file(private_key.unwrap(), SslFiletype::PEM)?;
+            ssl_connector.set_private_key_file(private_key.unwrap(), boring::ssl::SslFiletype::PEM)?;
 
             match (certificate, certificate_chain) {
                 (Some(cert), Some(chain)) => {
                     // assume cert is just a leaf and that we need to append the
                     // certs in the chain file after loading the leaf cert
 
-                    ssl_connector.set_certificate_file(cert, SslFiletype::PEM)?;
+                    ssl_connector.set_certificate_file(cert, boring::ssl::SslFiletype::PEM)?;
                     let pem = std::fs::read(chain)?;
-                    let chain = X509::stack_from_pem(&pem)?;
+                    let chain = boring::x509::X509::stack_from_pem(&pem)?;
                     for cert in chain {
                         ssl_connector.add_extra_chain_cert(cert)?;
                     }
@@ -60,13 +116,73 @@ impl Connector {
         let ssl_connector = ssl_connector.build();
 
         Ok(Connector {
-            inner: ConnectorImpl::TlsTcp(TlsTcpConnector {
+            inner: ConnectorImpl::BoringsslTlsTcp(BoringsslTlsTcp {
                 inner: ssl_connector,
                 verify_hostname: tls_config.verify_hostname(),
                 use_sni: tls_config.use_sni(),
             }),
         })
     }
+
+    #[cfg(feature = "openssl")]
+    fn openssl(config: &Config) -> Result<Self> {
+        if config.tls().is_none() {
+            return Ok(Connector {
+                inner: ConnectorImpl::Tcp,
+            });
+        }
+
+        let tls_config = config.tls().unwrap();
+
+        let private_key = tls_config.private_key();
+        let certificate = tls_config.certificate();
+        let certificate_chain = tls_config.certificate_chain();
+
+        let mut ssl_connector = openssl::ssl::SslConnector::builder(openssl::ssl::SslMethod::tls_client())?;
+
+        if let Some(ca_file) = tls_config.ca_file() {
+            ssl_connector.set_ca_file(ca_file)?;
+        }
+
+        // mTLS configuration
+        if private_key.is_some() && (certificate.is_some() || certificate_chain.is_some()) {
+            ssl_connector.set_private_key_file(private_key.unwrap(), openssl::ssl::SslFiletype::PEM)?;
+
+            match (certificate, certificate_chain) {
+                (Some(cert), Some(chain)) => {
+                    // assume cert is just a leaf and that we need to append the
+                    // certs in the chain file after loading the leaf cert
+
+                    ssl_connector.set_certificate_file(cert, openssl::ssl::SslFiletype::PEM)?;
+                    let pem = std::fs::read(chain)?;
+                    let chain = openssl::x509::X509::stack_from_pem(&pem)?;
+                    for cert in chain {
+                        ssl_connector.add_extra_chain_cert(cert)?;
+                    }
+                }
+                (Some(cert), None) => {
+                    // treat cert file like it's a chain for convenience
+                    ssl_connector.set_certificate_chain_file(cert)?;
+                }
+                (None, Some(chain)) => {
+                    // load all certs from chain
+                    ssl_connector.set_certificate_chain_file(chain)?;
+                }
+                (None, None) => unreachable!(),
+            }
+        }
+
+        let ssl_connector = ssl_connector.build();
+
+        Ok(Connector {
+            inner: ConnectorImpl::OpensslTlsTcp(OpensslTlsTcp {
+                inner: ssl_connector,
+                verify_hostname: tls_config.verify_hostname(),
+                use_sni: tls_config.use_sni(),
+            }),
+        })
+    }
+
 
     pub async fn connect(&self, addr: &str) -> Result<Stream> {
         match &self.inner {
@@ -80,7 +196,8 @@ impl Connector {
                     inner: StreamImpl::Tcp(s),
                 })
             }
-            ConnectorImpl::TlsTcp(connector) => {
+            #[cfg(feature = "boringssl")]
+            ConnectorImpl::BoringsslTlsTcp(connector) => {
                 let stream = tokio::net::TcpStream::connect(addr).await?;
                 let domain = addr.split(':').next().unwrap().to_owned();
 
@@ -92,11 +209,34 @@ impl Connector {
 
                 match tokio_boring::connect(config, &domain, stream).await {
                     Ok(stream) => Ok(Stream {
-                        inner: StreamImpl::TlsTcp(stream),
+                        inner: StreamImpl::BoringsslTlsTcp(stream),
                     }),
                     Err(e) => match e.as_io_error() {
-                        Some(e) => Err(Error::new(e.kind(), e.to_string())),
-                        None => Err(Error::new(ErrorKind::Other, e.to_string())),
+                        Some(e) => Err(std::io::Error::new(e.kind(), e.to_string())),
+                        None => Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string())),
+                    },
+                }
+            }
+            #[cfg(feature = "openssl")]
+            ConnectorImpl::OpensslTlsTcp(connector) => {
+                let stream = tokio::net::TcpStream::connect(addr).await?;
+                let domain = addr.split(':').next().unwrap().to_owned();
+
+                let config = connector
+                    .inner
+                    .configure()?
+                    .verify_hostname(connector.verify_hostname)
+                    .use_server_name_indication(connector.use_sni);
+
+                let mut ssl = tokio_openssl::SslStream::new(config.into_ssl(&domain)?, stream)?;
+
+                match tokio_openssl::SslStream::connect(std::pin::Pin::new(&mut ssl)).await {
+                    Ok(_) => Ok(Stream {
+                        inner: StreamImpl::OpensslTlsTcp(ssl),
+                    }),
+                    Err(e) => match e.io_error() {
+                        Some(e) => Err(std::io::Error::new(e.kind(), e.to_string())),
+                        None => Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string())),
                     },
                 }
             }
@@ -106,11 +246,22 @@ impl Connector {
 
 enum ConnectorImpl {
     Tcp,
-    TlsTcp(TlsTcpConnector),
+    #[cfg(feature = "boringssl")]
+    BoringsslTlsTcp(BoringsslTlsTcp),
+    #[cfg(feature = "openssl")]
+    OpensslTlsTcp(OpensslTlsTcp),
 }
 
-pub struct TlsTcpConnector {
+#[cfg(feature = "boringssl")]
+pub struct BoringsslTlsTcp {
     inner: boring::ssl::SslConnector,
+    verify_hostname: bool,
+    use_sni: bool,
+}
+
+#[cfg(feature = "openssl")]
+pub struct OpensslTlsTcp {
+    inner: openssl::ssl::SslConnector,
     verify_hostname: bool,
     use_sni: bool,
 }
@@ -121,7 +272,10 @@ pub struct Stream {
 
 enum StreamImpl {
     Tcp(tokio::net::TcpStream),
-    TlsTcp(tokio_boring::SslStream<tokio::net::TcpStream>),
+    #[cfg(feature = "boringssl")]
+    BoringsslTlsTcp(tokio_boring::SslStream<tokio::net::TcpStream>),
+    #[cfg(feature = "openssl")]
+    OpensslTlsTcp(tokio_openssl::SslStream<tokio::net::TcpStream>),
 }
 
 impl AsyncRead for Stream {
@@ -132,7 +286,10 @@ impl AsyncRead for Stream {
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_read(cx, buf),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_read(cx, buf),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_read(cx, buf),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_read(cx, buf),
         }
     }
 }
@@ -145,7 +302,10 @@ impl AsyncWrite for Stream {
     ) -> std::task::Poll<std::result::Result<usize, std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
         }
     }
     fn poll_flush(
@@ -154,7 +314,10 @@ impl AsyncWrite for Stream {
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_flush(cx),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
         }
     }
     fn poll_shutdown(
@@ -163,7 +326,10 @@ impl AsyncWrite for Stream {
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
         }
     }
 }
@@ -179,7 +345,13 @@ impl hyper::rt::Read for Stream {
                 let mut buf = ReadBuf::uninit(unsafe { rbc.as_mut() });
                 std::pin::Pin::new(s).poll_read(cx, &mut buf)
             }
-            StreamImpl::TlsTcp(s) => {
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => {
+                let mut buf = ReadBuf::uninit(unsafe { rbc.as_mut() });
+                std::pin::Pin::new(s).poll_read(cx, &mut buf)
+            }
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => {
                 let mut buf = ReadBuf::uninit(unsafe { rbc.as_mut() });
                 std::pin::Pin::new(s).poll_read(cx, &mut buf)
             }
@@ -195,7 +367,10 @@ impl hyper::rt::Write for Stream {
     ) -> std::task::Poll<std::result::Result<usize, std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_write(cx, buf),
         }
     }
 
@@ -205,7 +380,10 @@ impl hyper::rt::Write for Stream {
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_flush(cx),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_flush(cx),
         }
     }
 
@@ -215,7 +393,10 @@ impl hyper::rt::Write for Stream {
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
         match &mut self.inner {
             StreamImpl::Tcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
-            StreamImpl::TlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
+            #[cfg(feature = "boringssl")]
+            StreamImpl::BoringsslTlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
+            #[cfg(feature = "openssl")]
+            StreamImpl::OpensslTlsTcp(s) => std::pin::Pin::new(s).poll_shutdown(cx),
         }
     }
 }
