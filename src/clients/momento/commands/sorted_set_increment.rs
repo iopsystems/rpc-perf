733c443bf39e9b172df6c3498f0cc11ee2cc6769
Brian Martin <brayniac@gmail.com>
2024-07-02 08:32:08 -0700

    momento: re-add zincr and zmscore
    Re-adds support for sorted set incremenet and sorted set get scores now
    that they are in the reworked SDK

---
+++ src/clients/momento/commands/sorted_set_increment.rs
@@ -0,0 +1,27 @@
+use super::*;
+
+use ::momento::cache::{CollectionTtl, SortedSetIncrementScoreRequest};
+
+/// Increment the score for a member in a sorted set.
+///
+/// NOTE: if a TTL is specified, this command will not refresh the TTL for the
+/// collection.
+pub async fn sorted_set_increment(
+    client: &mut CacheClient,
+    config: &Config,
+    cache_name: &str,
+    request: workload::client::SortedSetIncrement,
+) -> std::result::Result<(), ResponseError> {
+    SORTED_SET_INCR.increment();
+
+    let r = SortedSetIncrementScoreRequest::new(cache_name, &*request.key, &*request.member, request.amount)
+        .ttl(CollectionTtl::new(request.ttl, false));
+
+    let result = timeout(
+        config.client().unwrap().request_timeout(),
+        client.send_request(r),
+    )
+    .await;
+    
+    record_result!(result, SORTED_SET_INCR)
+}
--- src/clients/momento/commands/sorted_set_score.rs
+++ src/clients/momento/commands/sorted_set_score.rs
@@ -9,18 +9,25 @@ pub async fn sorted_set_score(
 ) -> std::result::Result<(), ResponseError> {
     if request.members.is_empty() {
         return Ok(());
-    } else if request.members.len() > 1 {
-        REQUEST_UNSUPPORTED.increment();
-        return Ok(());
     }
 
     SORTED_SET_SCORE.increment();
 
-    let result = timeout(
-        config.client().unwrap().request_timeout(),
-        client.sorted_set_get_score(cache_name, &*request.key, &*request.members[0]),
-    )
-    .await;
+    if request.members.len() == 1 {
+        let result = timeout(
+            config.client().unwrap().request_timeout(),
+            client.sorted_set_get_score(cache_name, &*request.key, &*request.members[0]),
+        )
+        .await;
 
-    record_result!(result, SORTED_SET_SCORE)
+        record_result!(result, SORTED_SET_SCORE)
+    } else {
+        let result = timeout(
+            config.client().unwrap().request_timeout(),
+            client.sorted_set_get_scores(cache_name, &*request.key, request.members.iter().map(|f| &**f)),
+        )
+        .await;
+
+        record_result!(result, SORTED_SET_SCORE)
+    }
 }
