ae5289a9b38ceb9a737bad92670d8832f3d9a24d
Vir Ratanasangpunth <vir@momentohq.com>
2024-10-03 14:02:35 -0700

    lint lint

--- src/clients/cache/redis/commands/set.rs
+++ src/clients/cache/redis/commands/set.rs
@@ -17,13 +17,9 @@ pub async fn set(
 
     if let Some(ttl) = request.ttl {
         if ttl.subsec_nanos() == 0 {
-            command = command
-                .arg("EX")
-                .arg(ttl.as_secs());
+            command = command.arg("EX").arg(ttl.as_secs());
         } else {
-            command = command
-                .arg("PX")
-                .arg(ttl.as_millis() as u64);
+            command = command.arg("PX").arg(ttl.as_millis() as u64);
         }
     }
 
