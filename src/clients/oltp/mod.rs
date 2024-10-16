365bf99180fa935c4679269e2d53cf1bf7ffb449
Brian Martin <brayniac@gmail.com>
2024-10-16 10:14:32 -0700

    fixes for oltp clients

--- src/clients/oltp/mod.rs
+++ src/clients/oltp/mod.rs
@@ -11,18 +11,16 @@ pub fn launch(
     work_receiver: Receiver<ClientWorkItemKind<OltpRequest>>,
 ) -> Option<Runtime> {
     if config.oltp().is_none() {
-        debug!("No storage configuration specified");
+        debug!("No oltp configuration specified");
         return None;
     }
 
-    debug!("Launching clients...");
-
-    config.client()?;
+    debug!("Launching oltp clients...");
 
     // spawn the request drivers on their own runtime
     let mut client_rt = Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(config.client().unwrap().threads())
+        .worker_threads(config.oltp().unwrap().threads())
         .build()
         .expect("failed to initialize tokio runtime");
 
