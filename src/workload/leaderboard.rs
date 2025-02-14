44475d329f98612d3618621606b67467e5026321
Michael Landis <michael@momentohq.com>
2025-02-14 13:48:30 -0800

    chore: remove stale comments

--- src/workload/leaderboard.rs
+++ src/workload/leaderboard.rs
@@ -3,14 +3,12 @@ use std::sync::Arc;
 #[derive(Debug, PartialEq)]
 pub struct GetCompetitionRank {
     pub leaderboard: Arc<String>,
-    // TODO: should this be Arc?
     pub ids: Arc<[u32]>,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Upsert {
     pub leaderboard: Arc<String>,
-    // TODO: Modelled this after sorted set in client.rs
     pub elements: Vec<(u32, f64)>,
 }
 
